36d5ea4fa594f3fa46db13b4e0e5ea6ba5d87c6b
adriangz10 <adriiangz10@gmail.com>
2024-10-13 11:12:22 -0300

    agregando fecha de actualizacion

--- src/App.js
+++ src/App.js
@@ -1,76 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
-import Footer from './Footer';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import Footer from "./Footer";
 
 const App = () => {
-    const [valorVenta, setValorVenta] = useState(0);
-    const [montoDolares, setMontoDolares] = useState('');
-    const [equivalentePesos, setEquivalentePesos] = useState('-');
+  const [valorVenta, setValorVenta] = useState(0);
+  const [valorFecha, setFecha] = useState("");
+  const [montoDolares, setMontoDolares] = useState("");
+  const [equivalentePesos, setEquivalentePesos] = useState("-");
 
-    const apiUrl = 'https://dolarapi.com/v1/dolares/bolsa';
+  const apiUrl = "https://dolarapi.com/v1/dolares/bolsa";
 
-    // Función para obtener el valor de venta del dólar
-    const obtenerValorVenta = async () => {
-        try {
-            const response = await axios.get(apiUrl);
-            setValorVenta(response.data.venta); // Guarda el valor de venta
-        } catch (error) {
-            console.error('Error al obtener el valor de venta:', error);
-        }
-    };
+  const obtenerValorVenta = async () => {
+    try {
+      const response = await axios.get(apiUrl);
+      setValorVenta(response.data.venta);
+    } catch (error) {
+      console.error("Error al obtener el valor de venta:", error);
+    }
+  };
 
-    // Función para manejar el cambio en el input
-    const enviarDato = (event) => {
-        const valor = event.target.value;
-        setMontoDolares(valor);
-        const equivalente = (valor * valorVenta).toFixed(2); // Calcula el equivalente en pesos
-        setEquivalentePesos(equivalente); // Actualiza el equivalente
-    };
+  const obtenerFechaActualizacion = async () => {
+    try {
+      const response = await axios.get(apiUrl);
+      const fechaISO = response.data.fechaActualizacion;
 
-    useEffect(() => {
-        obtenerValorVenta();
-    }, []);
+      const fecha = new Date(fechaISO);
 
-    return (
-        <div className="container">
-            <header>
-                
-            </header>
+      const opciones = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      };
+      const fechaFormateada = fecha.toLocaleDateString("es-ES", opciones);
 
-            <section className="cotizacion">
-                <p>La cotización del Dólar MEP hoy es:</p>
-                <div className="precio">${valorVenta}</div>
-                {/* <p className="variacion">y tiene una variación del <span>0.00%</span> con respecto al día anterior.</p>
-                <button className="whatsapp-btn">Unite al WhatsApp Dólar Hoy</button> */}
-            </section>
+      setFecha(fechaFormateada);
+    } catch (error) {
+      console.error("Error al obtener el valor de la fecha", error);
+    }
+  };
 
-            <section className="conversor">
-                <h2>Conversor de Dólar MEP a pesos</h2>
-                <input
-                    type="number"
-                    value={montoDolares}
-                    placeholder="Ingresa monto en dólares"
-                    onChange={enviarDato}
-                />
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Tipo de Dólar</th>
-                            <th>Equivalente en Pesos</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Dólar MEP</td>
-                            <td>{equivalentePesos}</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
-            <Footer /> { Footer }
-        </div>
-    );
+  const enviarDato = (event) => {
+    const valor = event.target.value;
+    setMontoDolares(valor);
+    const equivalente = (valor * valorVenta).toFixed(2);
+    setEquivalentePesos(equivalente);
+  };
+
+  useEffect(() => {
+    obtenerValorVenta();
+    obtenerFechaActualizacion();
+  }, []);
+
+  return (
+    <div className="container">
+      <header></header>
+      <section className="cotizacion">
+        <p>La cotización del Dólar MEP hoy es:</p>
+        <div className="precio">${valorVenta}</div>
+        <p className="variacion">Ultima actualizacion {valorFecha}</p>
+        {/* <button className="whatsapp-btn">Unite al WhatsApp Dólar Hoy</button> */}
+      </section>
+      <section className="conversor">
+        <h2>Conversor de Dólar MEP a pesos</h2>
+        <input
+          type="number"
+          value={montoDolares}
+          placeholder="Ingresa monto en dólares"
+          onChange={enviarDato}
+        />
+        <table>
+          <thead>
+            <tr>
+              <th>Tipo de Dólar</th>
+              <th>Equivalente en Pesos</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Dólar MEP</td>
+              <td>{equivalentePesos}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <Footer /> {Footer}
+    </div>
+  );
 };
 
 export default App;
